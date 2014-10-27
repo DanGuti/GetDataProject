e48aff9dadbfaff2057ce2bcf69ed9b6718fd31f
Daniel <d.gutierrez.d@outlook.com>
2014-10-26 19:13:15 -0600

    Finished run_analysis.R
    Finished and debugged run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -22,7 +22,7 @@ trainy <- scan(tra)
 testS <- scan(tss)          ## data loading (Subjects)
 trainS <- scan(trs)
 
-test <- cbind(testS,testy,test  ## Bind subjects, activities and measurements
+test <- cbind(testS,testy,test)  ## Bind subjects, activities and measurements
 names(test)[1:2] <- c("Subject","Activity")
 train <- cbind(trainS,trainy,train)
 names(train)[1:2] <- c("Subject","Activity")
@@ -42,17 +42,38 @@ names(train)[1:2] <- c("Subject","Activity")
     mnst <- (mn | st) ##combine list of means and stds
 
     idx<-ft[,1] ##find variable numbers as idx
-    idx<-idx[mn]  ## retrieve index for mean or std factors
-     ## add 2 to each index value to account for Subject and activity columns
+    idx<-idx[mnst]  ## retrieve index for mean or std factors
+    idx=idx+2 ## add 2 to each index value to account for Subject and activity columns
 
-    Extract <- DF[,idx]#Extract measurments
-    
-
+    Extract <- DF[,c(1,2,idx)]#Extract measurments
 
 ## 3. Uses descriptive activity names to name the activities in the data set.
-
-
-
+    ## Read activity labels
+    actv <- "./UCI HAR Dataset/activity_labels.txt"
+    atv <-  read.table(actv,colClasses=c("integer","character"))
+    ## Define activities as factor with name of activity
+    Extract[,2] <- factor(Extract[,2],labels=atv[,2])
+    
 ## 4. Appropriately labels the data set with descriptive variable names. 
+    ## Retrieve list of variables with mean and std measurments
+    list <- ft[,2]
+    vars <- list[mnst]
+
+    ## use gsub to make variable names more explicit (using Camel Case for readability)
+    variables <- gsub("^[f]", "frequencyDomain", vars ) ##replace initial f for Frequency
+    variables <- gsub("^[t]", "timeDomain", variables ) ##replace initial t for time
+    variables <- gsub("Acc", "Acceleration", variables ) ##expand Acc eleration
+    variables <- gsub("Mag", "Magnitude", variables ) ##expand Mag nitude
+    variables <- gsub("-mean\\()", "Mean", variables ) ##Clean -mean()
+    variables <- gsub("-std\\()", "StandardDeviation", variables ) ##clean -std()
+    variables <- gsub("-X", "InX", variables ) ##clean-X
+    variables <- gsub("-Y", "InY", variables ) ##clean-Y
+    variables <- gsub("-Z", "InZ", variables ) ##clean-Z
+
+    names(Extract) <- c("Subject","Activity",variables)
 ## 5. From the data set in step 4, creates a second, independent tidy data set 
 ##    with the average of each variable for each activity and each subject.
+library(reshape2)
+resMelt <- melt(Extract,id=c("Subject","Activity"))
+result <- dcast(resMelt, Subject + Activity ~ variable,mean)
+write.table(result,"Result.txt",row.name=FALSE) ##Write results
